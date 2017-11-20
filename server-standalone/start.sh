3e8ece2b4539dd76a84993993e6a77f5b63d9fd7
Ernesto Barbosa <jose.barbosa@cwi.com.br>
2017-11-20 17:51:33 -0200

    cenarios soap, pdf e transformations

---
+++ server-standalone/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ "$OSTYPE" == "darwin"* || "$OSTYPE" == "linux-gnu" ]]; then
+    java -cp "server-standalone/wiremock-standalone-2.11.0.jar:server-standalone/wiremock-body-transformer-1.1.6.jar" com.github.tomakehurst.wiremock.standalone.WireMockServerRunner --verbose --extensions com.opentable.extension.BodyTransformer
+elif [[ "$OSTYPE" == "win32" || "$OSTYPE" == "msys" ]]; then
+	java -cp "server-standalone/wiremock-standalone-2.11.0.jar;server-standalone/wiremock-body-transformer-1.1.6.jar" com.github.tomakehurst.wiremock.standalone.WireMockServerRunner --verbose --extensions com.opentable.extension.BodyTransformer
+else
+    echo "Yo no lo coñosco señor..."
+fi
