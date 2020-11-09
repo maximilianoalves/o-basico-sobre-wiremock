818b366bd380efc7c22b3fb5247b2dd642a83013
Maximiliano <maximilianodacruz@gmail.com>
2020-11-09 12:32:01 -0300

    body transformer

--- server-standalone/start.sh
+++ server-standalone/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "darwin"* || "$OSTYPE" == "linux-gnu" ]]; then
-    java -cp "server-standalone/wiremock-standalone-2.11.0.jar:server-standalone/wiremock-body-transformer-1.1.6.jar" com.github.tomakehurst.wiremock.standalone.WireMockServerRunner --verbose --extensions com.opentable.extension.BodyTransformer
+    java -cp "server-standalone/wiremock-standalone-2.27.2.jar:server-standalone/wiremock-body-transformer-1.1.6.jar" com.github.tomakehurst.wiremock.standalone.WireMockServerRunner --verbose --local-response-templating --extensions com.opentable.extension.BodyTransformer
 elif [[ "$OSTYPE" == "win32" || "$OSTYPE" == "msys" ]]; then
 	java -cp "server-standalone/wiremock-standalone-2.11.0.jar;server-standalone/wiremock-body-transformer-1.1.6.jar" com.github.tomakehurst.wiremock.standalone.WireMockServerRunner --verbose --extensions com.opentable.extension.BodyTransformer
 else
